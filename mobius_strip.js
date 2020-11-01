60b1beb8f90db41ed360581a8ff362259ad9fbaa
20lives <lior@dotcore.co.il>
2020-11-01 14:36:08 +0200

    Basic examples

---
+++ mobius_strip.js
@@ -0,0 +1,14 @@
+const { cylinder, hull, union } = require('@dotcore64/scad-js');
+
+const wall = p => cylinder(20, 1)
+  .rotate([p, 0, 0])
+  .translate([0, 40, 0])
+  .rotate([0, 0, 2 * p]);
+
+const walls = [];
+
+for (let i = 0; i <= 360; i += 5) {
+    walls.push(hull(wall(i), wall(i + 5)));
+}
+
+console.log(union(...walls).serialize({ $fa: 50 }));
---
+++ turners_cube.js
@@ -0,0 +1,14 @@
+const { cylinder, cube, difference, union } = require('@dotcore64/scad-js');
+const cylinder1 = x => cylinder(12, x / 2);
+
+const cy3 = x => union(
+    cylinder1(x).rotate([90, 0, 0]),
+    cylinder1(x).rotate([0, 90, 0]),
+    cylinder1(x).rotate([0, 0, 90]),
+);
+
+const cutCube = x => difference(cube(x), cy3(x - 0.5));
+
+const output = union(...[4, 6, 8, 10].map(x => cutCube(x)));
+
+console.log(output.serialize({$fn: 20}));
